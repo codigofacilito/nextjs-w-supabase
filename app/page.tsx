6467a0ba3f5959dc650a1729e15d05c9c2800416
Juan Ortiz Jr <juanjo.jjo94@gmail.com>
2024-10-25 18:59:46 -0600

    Sesion Introduccion a Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
+import FirstComponentWSupabase from "./components/FirstComponentWSupabase";
+import FirstSCWSupabase from "./components/SCWSupabase";
+
 export default async function Index() {
   return (
     <>
+      <FirstComponentWSupabase />
+      <FirstSCWSupabase />
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
